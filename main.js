d91fd31c1608f02784c490f9dcd3eff77cf8b44c
acooler15 <renzongjia@vip.qq.com>
2020-10-14 17:54:57 +0800

    move to work

--- main.js
+++ main.js
@@ -1,35 +1,12 @@
-const wsgw = require('./lib/wsgw.js');
-const relx = require('./lib/relx.js');
-const xmyd = require('./lib/xmyd.js');
-const aliyun = require('./lib/aliyun2.js');
-const geekTime = require('./lib/geektime.js');
-const devdown = require('./lib/devdown.js');
-const ireader = require('./lib/ireader.js');
-const lagou = require('./lib/lagou.js');
-const unionpay = require('./lib/unionpay.js');
+const work = require('./work');
+const devdown = require('./devdown');
 
+for (let key in work) {
+  if (work.hasOwnProperty(key) && typeof work[key] == 'function') {
+    sleep(5000);
+    work[key]();
+  }
+}
 // sleep(5000);
-// aliyun();
-
-// sleep(5000);
-// geekTime();
-
-// sleep(5000);
-// lagou();
-
-// sleep(5000);
-// wsgw();
-
-sleep(5000);
-unionpay();
-
-// sleep(5000);
-// relx();
-
-// sleep(5000);
-// xmyd();
-
-// sleep(5000);
-// ireader();
-
-// devdown();
+// work.unionpay();
+devdown();
---
+++ work/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+  aliyun: require('./aliyun'),
+  geekTime: require('./geektime'),
+  lagou: require('./lagou'),
+  wsgw: require('./wsgw'),
+  unionpay: require('./unionpay'),
+  relx: require('./relx'),
+  xmyd: require('./xmyd'),
+  ireader: require('./ireader')
+}
