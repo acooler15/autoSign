8d21e353f51c82cd45af6025be733cb205a13d0d
acooler15 <acooler15>
2021-06-05 13:15:47 +0800

    更新拉勾教育、网上国网

--- work/wsgw.js
+++ work/wsgw.js
@@ -1,36 +1,27 @@
 const sign = function () {
   app.startActivity({
     packageName: 'com.sgcc.wsgw.cn',
-    // className: 'com.sgcc.wsgw.cn.activity.AdvertActivity',
     className: 'com.sgcc.wsgw.mainbundle.HomeActivity',
-    // className: 'org.zywx.wbpalmstar.engine.EBrowserActivity',
     root: true
   });
   sleep(7000);
+  home();
+  sleep(3000);
   shell('am force-stop com.sgcc.wsgw.cn', true);
   sleep(3000);
   app.startActivity({
     packageName: 'com.sgcc.wsgw.cn',
-    // className: 'com.sgcc.wsgw.cn.activity.AdvertActivity',
     className: 'com.sgcc.wsgw.mainbundle.HomeActivity',
-    // className: 'org.zywx.wbpalmstar.engine.EBrowserActivity',
     root: true
   });
   sleep(7000);
-
-  if (id('modelMove').exists()) {
-    id('modelMove').findOne().child(1).click();
-    sleep(5000);
-  }
-  if (id('im_view_signin').exists()) {
-    id('im_view_signin').findOne().click();
-    sleep(10000);
-  } else {
-    log('网上国网签到失败');
-  }
-  if (className('android.widget.Image').text('rightoff').exists()) {
-    className('android.widget.Image').text('rightoff').findOne().click();
-  } else {
+  let success = false;
+  id("item_house_bottom_integral_tv").find().forEach(child => {
+    var target = child.findOne(text('签到'));
+    target.click();
+    success = true;
+    });
+  if(!success){
     log('网上国网签到失败');
   }
   sleep(5000);
